11e4d13435aacb792ea24563e405265d87d68328
zheng-debug <18991224086@163.com>
2020-04-03 22:50:19 +0800

    提交的内容

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,11 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //取消配置数据库等操作
+@ServletComponentScan
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @MapperScan(value = "com.example.demo")//扫描指定包下面的所有Mapper文件
-public class DemoApplication {
+public class DemoApplication  extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
